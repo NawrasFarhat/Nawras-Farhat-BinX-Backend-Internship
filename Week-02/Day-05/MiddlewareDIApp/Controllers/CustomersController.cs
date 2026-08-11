e810daee3c7c1124c20eef144d7d217f7f732e28
NawrasFarhat <nawrasfarhat123@gmail.com>
2026-08-11 22:34:53 +0300

    Complete Week 02 Day 05 Middleware and Dependency Injection

---
+++ Week-02/Day-05/MiddlewareDIApp/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CustomersController:ControllerBase
+{
+    private readonly ICustomerService _customerService;
+    public CustomersController(ICustomerService customerService)
+    {
+        _customerService=customerService;
+    }
+    private readonly List<Customer>customers=new List<Customer>
+        {
+            new Customer{Id=1, Name="Nawras"},
+            new Customer{Id=2, Name="Sara"},
+            new Customer{Id=3, Name="Ahmad"}
+            
+        };
+[HttpGet]
+public List<Customer> GetCustomers()
+    {
+
+        return _customerService.GetCustomers();
+    }
+[HttpGet("{id}")]
+public Customer? GetCustomer(int id)
+{
+    var customer=customers.FirstOrDefault(c=>c.Id==id);
+    return customer;
+}
+}
