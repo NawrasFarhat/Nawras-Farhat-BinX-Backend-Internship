3bce19bc57c31b99a8ac512a935ed7d6f7e32a02
NawrasFarhat <nawrasfarhat123@gmail.com>
2026-08-12 17:29:51 +0300

    Complete Week 03 Day 01 REST API implementation

--- week-03/Day-01/RestApiDesignApp/Controllers/OrderController.cs
+++ week-03/Day-01/RestApiDesignApp/Controllers/OrderController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
@@ -16,4 +17,58 @@ public IActionResult GetOrders()
     {
         return Ok(orders);
     }
+[HttpGet("{id}")]
+
+public IActionResult GetOrders(int id)
+{
+    var order= orders.FirstOrDefault(o=>o.Id==id);
+    if(order==null)
+    {
+        return NotFound();
+    }
+    return Ok(order);
+
+}
+
+[HttpPost]
+public IActionResult CreateOrder(Order order)
+    {
+        orders.Add(order);
+        return CreatedAtAction(
+            nameof(GetOrders),
+            new{id=order.Id},
+            order
+        );
+    }
+[HttpPut("{id}")]
+public IActionResult UpdatOrder(int id, Order updateOrder)
+    {
+        var order= orders.FirstOrDefault(o=>o.Id==id);
+        if(order==null)
+        {
+            return NotFound();
+        }
+        order.Total=updateOrder.Total;
+        order.CustomerId=updateOrder.CustomerId;
+        return Ok(order);
+    }
+[HttpDelete("{id}")]
+public IActionResult DeleteOrder(int id)
+    {
+        var order= orders.FirstOrDefault(o=>o.Id==id);
+        if(order==null)
+        {
+            return NotFound();
+        }
+    orders.Remove(order);
+    return NoContent();
+        
+    }
+
+[HttpGet("/api/v1/customers/{CustomerId}/order")]
+public IActionResult GetCustomerOrders(int CustomerId)
+    {
+        var customerOrders= orders.Where(o=>o.CustomerId==CustomerId).ToList();
+        return Ok(customerOrders);
+    } 
 }
